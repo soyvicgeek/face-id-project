0509c313d5353640e8a35952fb33f0d63cb7d7c7
Daniel Flores <vflorestrejo@gmail.com>
2023-11-09 03:12:39 -0600

    Funcionabilidad de agregar

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,5 +4,12 @@ from usuarios.models import Usuario
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        #fields = ['usuario_clave', 'usuario_nombre']
-        fields = '__all__'
+        fields = [
+            'usuario_clave',
+            'usuario_nombre',
+            'usuario_apellido',
+            'usuario_correo',
+            'usuario_contrasena',
+            'usuario_role',
+        ]
+        #fields = '__all__'
