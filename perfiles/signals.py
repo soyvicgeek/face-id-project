e82679948dcf1e7fd34821cfc9bf6380020bdb45
Daniel Flores <vflorestrejo@gmail.com>
2023-11-30 01:04:04 -0600

    Fixed bug created profile

--- perfiles/signals.py
+++ perfiles/signals.py
@@ -4,6 +4,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Perfil.objects.create(user=instance)
+        Perfil.objects.create(usuario=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.perfil.save()
